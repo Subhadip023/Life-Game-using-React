6dce99577e17507db3beb5d48f26310e34b5fb14
Subhadip023 <subhadip240420@gmail.com>
2024-07-18 21:27:17 +0530

    2d array done

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,22 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+function array_2D(row,cols) {
+  let array=[]
+  for (let index = 0; index < row; index++) {
+     array[i]=[]
+    for (let j = 0; j < cols; j++) {
+      const element = array[j];
+      
+    }
+    
+  }
+  return array;
+}
 
   return (
     <>
-    <h1>
-      Life game | Subhadip
-    </h1>
+   <section
     
     </>
   )
